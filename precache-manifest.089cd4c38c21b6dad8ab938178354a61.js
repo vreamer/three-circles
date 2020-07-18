01aa8ac1568c00aa9aad0b6b1dd1955ec4ccdec8
Vreamer <yhou.vreamer@gmail.com>
2020-07-18 13:38:43 +0800

    Deploy

---
+++ precache-manifest.089cd4c38c21b6dad8ab938178354a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3add92e088909aa83cba46035c3f9c2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5d154af5ee5fe20367",
+    "url": "/static/css/main.e286bcb8.chunk.css"
+  },
+  {
+    "revision": "fae66825a7ccb65458f9",
+    "url": "/static/js/2.79d63ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79d63ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d154af5ee5fe20367",
+    "url": "/static/js/main.d602eb35.chunk.js"
+  },
+  {
+    "revision": "196382e3084ee1ea1d83",
+    "url": "/static/js/runtime-main.6fea72c3.js"
+  }
+]);
