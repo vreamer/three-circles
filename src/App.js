d0b4a8b027e7376fca4a5e008c2d74b8bbc07938
Vreamer <yhou.vreamer@gmail.com>
2020-07-20 10:38:50 +0800

    Light up overlapped circles when user clicks on intersection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+
+const CIRCLE_RADIUS = 150
+const INTERSECT_OFFSET = 25
+const CIRCLE_STROKE_WIDTH = 3
+const width = 2 * CIRCLE_RADIUS + (CIRCLE_RADIUS - INTERSECT_OFFSET) * 1.732
+const height = 2 * CIRCLE_RADIUS + (CIRCLE_RADIUS - INTERSECT_OFFSET) * 1.5
 
 function App() {
   const [goal, setGoal] = useState(false)
   const [action, setAction] = useState(false)
   const [knowledge, setKnowledge] = useState(false)
 
+  const onCircleClick = (e) => {
+    const elements = document.elementsFromPoint(e.pageX, e.pageY)
+    const selectedCircles = elements.filter(e => e.nodeName === 'circle' && e.className.baseVal.indexOf('menu-circle') >= 0)
+    selectedCircles.forEach((c) => {
+      switch (c.id) {
+        case 'menu-circle-goals':
+          setGoal(!goal)
+          break
+        case 'menu-circle-actions':
+          setAction(!action)
+          break
+        case 'menu-circle-knowledge':
+          setKnowledge(!knowledge)
+          break
+        default:
+          console.log('circle does not belong to menu')
+      }
+    })
+  }
+
   return (
     <div className='app-container'>
       <h3>Three Circles</h3>
-      <div className='three-circles-container'>
-        <div onClick={() => setGoal(!goal)} className={'goal-circle' + (goal ? ' selected' : '')}></div>
-        <div onClick={() => setAction(!action)} className={'action-circle' + (action ? ' selected' : '')}></div>
-        <div onClick={() => setKnowledge(!knowledge)} className={'knowledge-circle' + (knowledge ? ' selected' : '')}></div>
-      </div>
+      <svg width={width} height={height}>
+        <circle id='menu-circle-goals' className={'menu-circle goals ' + (goal ? 'selected' : '')} onClick={onCircleClick} cx={width / 2} cy={CIRCLE_RADIUS} r={CIRCLE_RADIUS - CIRCLE_STROKE_WIDTH} />
+        <circle id='menu-circle-actions' className={'menu-circle actions ' + (action ? 'selected' : '')} onClick={onCircleClick} cx={CIRCLE_RADIUS} cy={height - CIRCLE_RADIUS} r={CIRCLE_RADIUS - CIRCLE_STROKE_WIDTH} />
+        <circle id='menu-circle-knowledge' className={'menu-circle knowledge ' + (knowledge ? 'selected' : '')} onClick={onCircleClick} cx={width - CIRCLE_RADIUS} cy={height - CIRCLE_RADIUS} r={CIRCLE_RADIUS - CIRCLE_STROKE_WIDTH} />
+      </svg>
     </div>
   );
 }
